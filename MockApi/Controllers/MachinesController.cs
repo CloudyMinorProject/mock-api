b2e3a7e711092789ff92e1fccb5f2f038a349b57
Elizabeth <elizabeth.erickson21@gmail.com>
2025-11-05 15:03:34 +0100

    removed using error

--- MockApi/Controllers/MachinesController.cs
+++ MockApi/Controllers/MachinesController.cs
@@ -1,21 +1,30 @@
-using System.Reflection.PortableExecutable;
+//using System.Reflection.PortableExecutable;
 using Microsoft.AspNetCore.Mvc;
+using MockApi.Models;
 
 namespace MockApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+[ProducesResponseType(typeof(List<Machine>), StatusCodes.Status200OK)] 
 public class MachinesController : ControllerBase
 {
-    private List<Machine> machinesArray = new List<Machine>();
+    private List<Machine> machinesList = new List<Machine>();
 
 
 
     [HttpGet(Name = "GetMachines")]
     public List<Machine> GetMachines()
     {
+        Machine machineTest = new Machine();
+        machineTest.MachineID = 1;
+        machineTest.MachineName = "test machine";
+
+        machinesList.Add(machineTest);
+        
+
         //Add info from database
-        return machinesArray;
+        return machinesList;
 
     }
 }
