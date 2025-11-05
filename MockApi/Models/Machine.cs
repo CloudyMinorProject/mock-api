d83aaa44d2ee4be9c260fa746866a8c552291726
Elizabeth <elizabeth.erickson21@gmail.com>
2025-11-05 11:30:20 +0100

    removed weatherforcasts

--- MockApi/Models/Machine.cs
+++ MockApi/Models/Machine.cs
@@ -2,6 +2,12 @@ namespace MockApi.Models
 {
     public class Machine
     {
-
+        public int MachineID { get; set; }
+        public string MachineName { get; set; }
+        public int AccespointID { get; set; }
+        public int DeviceSerialNr { get; set; }
+        public int ZigbeeSerialNrHigh { get; set; }
+        public int ZigbeeSerialNrLow { get; set; }
+        
     }
 }
